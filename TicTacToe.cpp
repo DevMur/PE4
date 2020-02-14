591360a89f4bbd0f64407d01d32ada7338639d0a
Devin Murray <devin.murray@colorado.edu>
2020-02-13 20:30:25 -0700

    My display board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,7 +3,19 @@
 #include <sstream>
 using namespace std;
 
-//master version
+//Devins version
+void DisplayBoard(vector<vector<int>> current_board) //takes the current board as input
+{
+    for (int i = 0; i < 3; i++) //3's are faster than vector.size() calls. It'll always be 3
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << current_board[i][j] << " "; //prints the row, no endl
+        }
+        cout << endl; //once a row exits an endl is used
+    }
+}
+
 
 //Fill the board with 0's, 1's can be X's and 2's can be O's
 //-1 as a return or at a location can denote failure.
@@ -29,4 +41,6 @@ int main()
     //a tictactoe board does not require anything too creative. It is a 3x3 matrix
     //nothing more, nothing less.
     vector<vector<int>> board = createBoard(); 
+    //board[1][1] = 8; //used to test memory
+    DisplayBoard(board); 
 }
